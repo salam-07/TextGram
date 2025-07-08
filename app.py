ffe4e11ac7917a50d9237c55319b13b4cb738856
salam-07 <abdussalamblue7@gmail.com>
2025-07-08 15:33:44 +0500

    Add Routes and Dummy Data

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
+# dummy data is a list of dictionaries
+posts = [
+    {
+        'author': 'Abdus Salam',
+        'content': 'This App is so lit',
+        'date_posted': '15:02, 8 July 2025'
+    }
+    ,
+    {
+        'author': 'Joe Goldberg',
+        'content': 'Hello, You',
+        'date_posted': '14:30, 4 July 2025'
+    }
+]
+
+
 app = Flask(__name__)
 
 # both routes lead to homepage
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
 
 # about page route
 @app.route('/about')

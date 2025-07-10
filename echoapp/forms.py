cd6703932be7a0e28a999d6a8eabd904dd8faf3d
salam-07 <abdussalamblue7@gmail.com>
2025-07-10 15:08:06 +0500

    Add Profile Pics

--- echoapp/forms.py
+++ echoapp/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
@@ -24,10 +25,11 @@ class LoginForm(FlaskForm):
 
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
-    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png', 'jpeg'])]) 
     submit = SubmitField('Update')
 
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('That Username is taken already!')
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That Username is taken already!')
--- echoapp/routes.py
+++ echoapp/routes.py
@@ -1,8 +1,11 @@
 from flask import render_template, url_for, redirect, flash, request
 from echoapp import app, db, bcrypt
 from echoapp.models import User, Post
-from echoapp.forms import RegistrationForm, LoginForm
+from echoapp.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+import os
+from PIL import Image
 
 # dummy data is a list of dictionaries
 posts = [
@@ -59,13 +62,43 @@ def login():
             flash("Couldn't Log in! Check username and password.", 'danger')
     return render_template('login.html', title="Log In", form=form)
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    form_picture.save(i)
+    
+    return picture_fn
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+    
+        current_user.username = form.username.data
+        db.session.commit()
+        flash('Your Account has been updated!', 'success')
+        redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+
     image_file = url_for('static', filename="profile_pics/" + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route('/post/echo')
+@login_required
+def new_echo():
+    render_template('create_echo.html', title='Share an Echo')

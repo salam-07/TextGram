5fbf5feecdfd9d7ee7e7e960e041831ed445538a
Abdus Salam Ameen <abdussalamblue7@gmail.com>
2025-07-14 19:57:16 +0500

    Add Private Network Testing

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from echoapp import app
 
 if __name__ == "__main__":
     # debug=True for reflecting changes to server on save
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
